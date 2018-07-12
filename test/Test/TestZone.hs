d1c68cdf1ddc81cc387088807ce4e8f977546acb
Imants Cekusins <imantc@gmail.com>
2018-07-12 08:50:11 +0300

    tests

--- test/Test/TestZone.hs
+++ test/Test/TestZone.hs
@@ -8,6 +8,7 @@ import Data.Time.LocalTime.TimeZone.Series
 import Data.Time.Hora.Stamp
 import Data.Time.Hora.Type
 import Data.Time.Hora.Part
+import Data.Time.Hora.Zone
 
 
 main::IO()
@@ -18,9 +19,13 @@ main = hspec $ do
             china1 <- getTimeZoneSeriesFromOlsonFile "./test/TimeSeriesData/ROC"
             now1 <- now::IO UTCTime 
             traceIO $ "UTC: " ++ (show now1)
-            traceIO $ "Chile: " ++ (show $ ttz chile1 now1)
-            traceIO $ "China: " ++ (show $ ttz china1 now1)
-            1 `shouldBe` 1
+            let chile2 = ttz chile1 now1
+                china2 = ttz china1 now1
+                china3 = toTimeZone china1 chile2
+            traceIO $ "Chile: " ++ (show chile2)
+            traceIO $ "China: " ++ (show china1)
+            traceIO $ "China converted: " ++ (show china3)
+            Just china2 `shouldBe` china3
 
 
 ttz::TimeZoneSeries -> UTCTime -> Tz (DatePart Int)
