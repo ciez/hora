92e4a1ae598c26cac43b46ba4f5d9c6af86ad129
Imants Cekusins <imantc@gmail.com>
2018-08-15 15:32:15 +0300

    test UTCTimeBin

--- test/Main.hs
+++ test/Main.hs
@@ -1,17 +1,18 @@
 module Main where
 
-import qualified Test.TestDiffTime as T
 import qualified Test.TestConvert as Conv
+import qualified Test.TestDiffTime as T
+import qualified Test.TestDmyHm as D
+import qualified Test.TestDatePart as Dp
+import qualified Test.TestUtc as U
 import qualified Test.TestFuture as Fut
-import qualified Test.TestTime as Tt
 import qualified Test.TestPico as P
 import qualified Test.TestPico2 as P2
-import qualified Test.TestDmyHm as D 
-import qualified Test.TestDatePart as Dp 
-import qualified Test.TestUtc as U
-import qualified Test.TestZone as Z
-import qualified Test.TestSome as Tcom 
+import qualified Test.TestSome as Tcom
 import qualified Test.TestFormat as Tf
+import qualified Test.TestTime as Tt
+import qualified Test.TestUTCTimeBin as Bin
+import qualified Test.TestZone as Z
 
 
 main::IO()
@@ -28,3 +29,4 @@ main = do
         Z.main
         Tcom.main
         Tf.main
+        Bin.main
---
+++ test/Test/TestUTCTimeBin.hs
@@ -0,0 +1,30 @@
+module Test.TestUTCTimeBin where
+
+import Data.Binary
+import Data.ByteString.Lazy
+import Data.Maybe
+import Data.Time.Hora.Part
+import Data.Time.Hora.Stamp
+import Data.Time.Hora.Type
+import Debug.Trace
+import Test.Hspec
+
+
+main::IO()
+main = hspec $ do
+   describe "round trip convert & serialize UTCTime via UTCTimeBin" $ do
+      it "round trip convert" $ do
+         utc1 <- now
+         let utcbin2 = fromUtc utc1::UTCTimeBin
+             utc2 = fromJust $ toUtc utcbin2
+         traceIO $ show utc1
+         traceIO $ show utcbin2
+         traceIO $ show utc2
+         utc2 `shouldBe` utc1
+      it "round trip convert & serialize" $ do
+         utc10 <- now
+         let utcbin10 = fromUtc utc10::UTCTimeBin
+             bin11 = encode utcbin10::ByteString
+             utcbin12 = decode bin11::UTCTimeBin
+             utc13 = fromJust $ toUtc utcbin12
+         utc10 `shouldBe` utc13
