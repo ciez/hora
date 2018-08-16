a3ef16e9af5fee84b057e7bb88724dc922038a51
Imants Cekusins <imantc@gmail.com>
2018-08-16 16:50:45 +0300

    TestStorageSize

--- test/Test/TestStorageSize.hs
+++ test/Test/TestStorageSize.hs
@@ -19,18 +19,28 @@ main = hspec $ do
       it "Min 10" $ do
          let result1 = Min 10
              bin2 = encode result1
-         traceIO $ "Min10 size is " <> (show $ B.length $ B.concat $ L.toChunks bin2)
+         tr1 "Min 10 size is " bin2
+         1 `shouldBe` 1
+      it "Day 10" $ do
+         let result1 = Day 10
+             bin2 = encode result1
+         tr1 "Day 10 size is " bin2
+         1 `shouldBe` 1
+      it "Time 1 2" $ do
+         let result1 = Time 1 2
+             bin2 = encode result1
+         tr1 "Time 1 2 size is " bin2
          1 `shouldBe` 1
       it "DatePartSmall 10 2 1" $ do
          let result10 = DatePartSmall 10 2 1
              bin11 = encode result10
-         traceIO $ "DatePartSmall size is " <> (show $ B.length $ B.concat $ L.toChunks bin11)
+         tr1 "DatePartSmall size is " bin11
          1 `shouldBe` 1
       it "DatePartSmall from now" $ do
          now20 <- now::IO UTCTime
          let result21 = fromUtc now20::DatePartSmall
              bin22 = encode result21
-         traceIO $ "DatePartSmall size is " <> (show $ B.length $ B.concat $ L.toChunks bin22)
+         tr1 "DatePartSmall size is " bin22
          1 `shouldBe` 1
       it "Int64" $ do
          let result21 = 10::Int64
