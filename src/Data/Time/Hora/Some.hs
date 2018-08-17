1279ddaa1e59bf39b3ff5f4a55dfefe31ee44658
Imants Cekusins <imantc@gmail.com>
2018-08-17 15:26:37 +0300

    show' + tests

--- src/Data/Time/Hora/Some.hs
+++ src/Data/Time/Hora/Some.hs
@@ -1,7 +1,10 @@
 {-| common format shortcuts -}
 module Data.Time.Hora.Some where
 
+import Data.Maybe
 import Data.Time.Hora.Format
+import Data.Time.Hora.Internal.DatePartSmall as S
+import Data.Time.Hora.Part
 
 
 {- | yyyy-mm-dd  -}
@@ -34,3 +37,35 @@ t = [Hms]
 2016-12-14 09:16:23       -}
 dt::[Format]
 dt = [Y_m_d, Raw " ", Hms]
+
+
+{- | pretty print 'DatePartSmall'
+
+incremental values are prefixed with + or -
+-}
+show'::DatePartSmall -> String
+show' dp0
+   | (Day d0) <- dp0
+         = format iso $ fromJust $ toUtc $ DatePartSmall d0 0 0
+   | (Day' d0) <- dp0
+         = "+" <> (show d0) <> " days"
+   | (S.Min m0) <- dp0
+         = let hr1 = m0 `div` 60
+               min1 = m0 `rem` 60
+           in show hr1 <> ":" <> show min1
+   | (S.Min' m0) <- dp0
+         = "+" <> (show' $ S.Min m0)
+   | (S.Ms ms0) <- dp0
+         = let sec1 = ms0 `div` 1000
+               ms1 = ms0 `rem` 1000
+           in show sec1 <> "." <> show ms1
+   | (S.Ms' ms0) <- dp0
+         = "+" <> (show' $ S.Ms ms0)
+   | (S.Time m0 ms0) <- dp0
+        = show' (S.Min m0) <> ":" <> show' (S.Ms ms0)
+   | (S.DatePartSmall d0 m0 ms0) <- dp0
+        = show' (S.Day d0) <> " " <> show' (S.Min m0) <> ":" <> show' (S.Ms ms0)
+   | (S.Neg dp0) <- dp0
+        = let _:s1 = show' dp0
+          in "-" <> s1
+   | otherwise = show dp0
--- test/Test/TestFormat.hs
+++ test/Test/TestFormat.hs
@@ -1,8 +1,10 @@
 module Test.TestFormat where
 
 import Data.Time.Hora.Format
+import Data.Time.Hora.Part
+import Data.Time.Hora.Some
 import Data.Time.Hora.Stamp
-import Data.Time.Hora.Type
+import Data.Time.Hora.Type as T
 import Debug.Trace
 import Test.Hspec
 
@@ -28,6 +30,36 @@ main = hspec $ do
           it "week of year" $ do
             ts [Wk_year_Mon, " ", Wk_year_Sun] >>= utc
             1 `shouldBe` 1
+       describe "TestFormat show DatePartSmall" $ do
+          it "Day" $ do
+            show' (mkDay 2018 08 17) `shouldBe` "2018-08-17"
+          it "Min" $ do
+            show' (mkMin 14 53) `shouldBe` "14:53"
+          it "Ms" $ do
+            show' (mkMs 7 358) `shouldBe` "7.358"
+          it "Time" $ do
+            let (T.Min m1) = mkMin 15 17
+                (T.Ms ms1) = mkMs 7 358
+            show' (T.Time m1 ms1) `shouldBe` "15:17:7.358"
+          it "DatePartSmall" $ do
+            let (T.Day d1) = mkDay 2018 08 17
+                (T.Min m1) = mkMin 15 17
+                (T.Ms ms1) = mkMs 7 358
+            show' (T.DatePartSmall d1 m1 ms1) `shouldBe` "2018-08-17 15:17:7.358"
+          it "Day'" $ do
+            show' (Day' 3) `shouldBe` "+3 days"
+          it "Min'" $ do
+            show' (toSpan $ mkMin 14 53) `shouldBe` "+14:53"
+            show' (toSpan $ mkMin 0 53) `shouldBe` "+0:53"
+          it "Ms'" $ do
+            show' (toSpan $ mkMs 7 358) `shouldBe` "+7.358"
+          it "Neg Day'" $ do
+            show' (T.negate $ Day' 3) `shouldBe` "-3 days"
+          it "Neg Min'" $ do
+            show' (T.negate $ toSpan $ mkMin 14 53) `shouldBe` "-14:53"
+            show' (T.negate $ toSpan $ mkMin 0 53) `shouldBe` "-0:53"
+          it "Neg Ms'" $ do
+            show' (T.negate $ toSpan $ mkMs 7 358) `shouldBe` "-7.358"
 
 
 utc::Tz String -> IO()
